cfd449617a44dd05a0a23134cf568f59e99c3183
mikefaraponov <java.math.random@bk.ru>
2016-06-15 19:09:54 +0300

    Added net to the canvas

--- assets/js/arkanoid.js
+++ assets/js/arkanoid.js
@@ -1,5 +1,9 @@
 var canvas;
 var ctx;
+var ballX = 50;
+var ballY = 50;
+var ballSpeedX = 10;
+var ballSpeedY = 5;
 
 function moveEverything() {
 
@@ -7,12 +11,22 @@ function moveEverything() {
 
 function drawEverything() {
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
+
+	drawNet();
+
+}
+
+function drawNet() {
+	for(var i = 0; i < canvas.height; i += 40) {
+		colorRect(canvas.width / 2-1, i, 2, 20, 'white');
+	}
 }
 
 function colorRect(leftX, topY, width, height, drawColor) {
 	ctx.fillStyle = drawColor;
 	ctx.fillRect(leftX,topY, width,height);
 }
+
 
 window.onload = function() {
 	canvas = document.querySelector('.js-arkanoid');
