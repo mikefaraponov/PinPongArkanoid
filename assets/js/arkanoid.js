1f08bfe4681a654b51f904f7614ad60a8da64259
mikefaraponov <java.math.random@bk.ru>
2016-06-15 19:04:45 +0300

    Written main game loop and rectangle render util

--- assets/js/arkanoid.js
+++ assets/js/arkanoid.js
@@ -1,1 +1,29 @@
-console.info('arkanoid.js');
+var canvas;
+var ctx;
+
+function moveEverything() {
+
+}
+
+function drawEverything() {
+	colorRect(0, 0, canvas.width, canvas.height, 'black');
+}
+
+function colorRect(leftX, topY, width, height, drawColor) {
+	ctx.fillStyle = drawColor;
+	ctx.fillRect(leftX,topY, width,height);
+}
+
+window.onload = function() {
+	canvas = document.querySelector('.js-arkanoid');
+	canvas.height = 500;
+	canvas.width = 700;
+
+	ctx = canvas.getContext('2d');
+
+	var fps = 30;
+	setInterval(function() {
+			moveEverything();
+			drawEverything();
+	}, 1000/fps);
+}
